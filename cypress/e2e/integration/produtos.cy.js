6dba531e277d25da80e10a79ca12669c2aa8d49e
Silvana Bierhals <silbierhals@gmail.com>
2023-04-14 10:20:52 +0100

    alteracoes

---
+++ cypress/e2e/integration/produtos.cy.js
@@ -0,0 +1,35 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade Pagina de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get("[class='product-block grid']")
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+    });
+
+    it.only('Deve adicionar o produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get("[class='product-block grid']")
+                .contains('Atlas Fitness Tank').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+
+
+
+    });
+});

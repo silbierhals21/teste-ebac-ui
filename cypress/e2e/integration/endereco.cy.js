8d6683c7fa85766704dc7692a91aca8059aa0e0c
Silvana Bierhals <silbierhals@gmail.com>
2023-04-23 22:26:36 +0100

    Usando Arquivo de dados

--- cypress/e2e/integration/endereco.cy.js
+++ cypress/e2e/integration/endereco.cy.js
@@ -1,5 +1,6 @@
 ///<reference types="cypress"/>
 import EnderecoPage from '../../support/page-objects/endereco.page'
+const dadosEndereco = require ('../../fixtures/endereco.json')
 
 describe('Funcionalidade Enderecos - Faturamento e entrega', () => {
     beforeEach(() => {
@@ -14,4 +15,27 @@ describe('Funcionalidade Enderecos - Faturamento e entrega', () => {
         EnderecoPage.editarEnderecoFaturamento('Ana', 'Furtado', 'ABC Company', 'Brasil', 'Av Rio Claro', '500', 'Sao Jose', 'Santa Catarina', '88000111', '48999998888', 'anafurtado@teste.com' )  
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')   
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].endereco,
+            dadosEndereco[0].numero,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cep,
+            dadosEndereco[0].telefone,
+            dadosEndereco[0].email,
+           )  
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')   
+    });
+
+    it.only('Deve fazer cadastro de entrega com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega('Maria', 'Leal', 'SD Company', 'Brasil', 'Av Rio Verde', '122', 'Biguacu', 'Santa Catarina', '88000111')  
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    });
+
 });
--- cypress/e2e/integration/pre-cadastro.cy.js
+++ cypress/e2e/integration/pre-cadastro.cy.js
@@ -25,7 +25,7 @@ describe('Funcionalidade Pre Cadastro', () => {
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 
-    it.only('Deve completar o pre-cadastro com sucesso - Usando Comandos Customizados', () => {
+    it('Deve completar o pre-cadastro com sucesso - Usando Comandos Customizados', () => {
         let emailFaker2 = faker.internet.email()
         cy.preCadastro(emailFaker2, 'senha@forte', 'Silvana', 'Bierhals')
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')

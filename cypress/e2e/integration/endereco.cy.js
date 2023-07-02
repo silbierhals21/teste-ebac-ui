ae825df9ff4438216cb0e356c6c29325720e0ae0
Silvana Bierhals <silbierhals@gmail.com>
2023-07-02 10:02:44 +0100

    ultimas atualizacoes

--- cypress/e2e/integration/endereco.cy.js
+++ cypress/e2e/integration/endereco.cy.js
@@ -1,19 +1,19 @@
 ///<reference types="cypress"/>
 import EnderecoPage from '../../support/page-objects/endereco.page'
-const dadosEndereco = require ('../../fixtures/endereco.json')
+const dadosEndereco = require('../../fixtures/endereco.json')
 
 describe('Funcionalidade Enderecos - Faturamento e entrega', () => {
     beforeEach(() => {
         cy.visit('minha conta')
-        cy.fixture('perfil').then (dados => {
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
+
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento('Ana', 'Furtado', 'ABC Company', 'Brasil', 'Av Rio Claro', '500', 'Sao Jose', 'Santa Catarina', '88000111', '48999998888', 'anafurtado@teste.com' )  
-        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')   
+        EnderecoPage.editarEnderecoFaturamento('Ana', 'Furtado', 'ABC Company', 'Brasil', 'Av Rio Claro', '500', 'Sao Jose', 'Santa Catarina', '88000111', '48999998888', 'anafurtado@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
     it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
@@ -29,13 +29,27 @@ describe('Funcionalidade Enderecos - Faturamento e entrega', () => {
             dadosEndereco[0].cep,
             dadosEndereco[0].telefone,
             dadosEndereco[0].email,
-           )  
-        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')   
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
     it.only('Deve fazer cadastro de entrega com sucesso', () => {
-        EnderecoPage.editarEnderecoEntrega('Maria', 'Leal', 'SD Company', 'Brasil', 'Av Rio Verde', '122', 'Biguacu', 'Santa Catarina', '88000111')  
+        EnderecoPage.editarEnderecoEntrega('Maria', 'Leal', 'SD Company', 'Brasil', 'Av Rio Verde', '122', 'Biguacu', 'Santa Catarina', '88000111')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    });
+    it('Deve fazer cadastro de Entrega com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
 });
